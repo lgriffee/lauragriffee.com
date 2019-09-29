a8f2ae620ee2c47d4b7e7da84c11f468832282fe
Laura Griffee <laura@mailzone.com>
2019-09-28 20:06:52 -0600

    feat: Create intro section on homepage

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,20 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="nav"></nav>
+      <main>
+        <section className="intro">
+          <header>
+            <h1 className="intro__name">Laura Griffee.</h1>
+          </header>
+
+          <p className="intro__about">
+            I’m a UI/UX Developer skilled at user experience design (wireframing, prototyping, and testing), interface design, and front-end development. I love ensuring designs deliver value to users and that development delivers on that vision. I get excited about maintaining design systems, building products end-to-end, and bridging the gap between design and engineering.
+          </p>
+
+          <p className="intro__links">
+            <a title="LinkedIn" href="https://linkedin.com/in/laura-griffee/">LinkedIn</a>&nbsp;&nbsp;/&nbsp;&nbsp;<a title="GitHub" href="https://github.com/lgriffee/">GitHub</a>
+          </p>
+
+        </section>
+      </main>
+      <footer className="footer"></footer>
     </div>
   );
 }
