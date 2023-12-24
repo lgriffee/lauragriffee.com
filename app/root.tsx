bf1efaf2b0fbc0de1c67cf657d007cd666c429bc
Laura Griffee <laura@mailzone.com>
2023-12-24 17:32:21 -0500

    feat: Add favicons

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,25 @@ import {
 } from '@remix-run/react';
 
 export const links: LinksFunction = () => [
+  {
+    rel: 'icon',
+    sizes: '64x64 32x32 24x24 16x16',
+    href: '/favicon.ico',
+    type: 'image/vnd.microsoft.icon',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '192x192',
+    href: '/favicons/laura-griffee-logo-192.png',
+    type: 'image/png',
+  },
+  {
+    rel: 'icon',
+    sizes: '512x512',
+    href: '/favicons/laura-griffee-logo-512.png',
+    type: 'image/png',
+  },
+  {rel: 'manifest', href: '/manifest.json'},
   ...(cssBundleHref ? [{rel: 'stylesheet', href: cssBundleHref}] : []),
 ];
 
@@ -28,6 +47,7 @@ export default function App() {
         <Scripts />
         <LiveReload />
       </body>
+      <LiveReload />
     </html>
   );
 }
